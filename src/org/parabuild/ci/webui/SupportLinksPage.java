dae4808fbf4943d7910845e43c4c291b51579e73
Slava Imeshev <imeshev@yahoo.com>
2019-02-28 20:49:17 -0800

    Code quality: fixed SCA issues.

--- src/org/parabuild/ci/webui/SupportLinksPage.java
+++ src/org/parabuild/ci/webui/SupportLinksPage.java
@@ -17,14 +17,14 @@ import org.parabuild.ci.webui.common.*;
 import viewtier.ui.*;
 
 /**
- * This class is a admin support repsources
+ * This class is a admin support resources.
  */
 public final class SupportLinksPage extends BasePage implements StatelessTierlet {
 
-  static final long serialVersionUID = 644441224120456213L; // NOPMD
+  private static final long serialVersionUID = 644441224120456213L; // NOPMD
 
-  public static final String VIEWTIER_SUPPORT_HOME_URL = "http://www.parabuildci.org/support.htm";
-  public static final String VIEWTIER_SUPPORT_HOME_CAPTION = "Parabuild CI technical support home";
+  private static final String VIEWTIER_SUPPORT_HOME_URL = "http://www.parabuildci.org/support.htm";
+  private static final String VIEWTIER_SUPPORT_HOME_CAPTION = "Parabuild CI technical support home";
 
   /**
    * Constructor.
