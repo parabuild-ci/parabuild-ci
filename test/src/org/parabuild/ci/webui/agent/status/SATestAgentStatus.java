aba1d62f1ccfe5a52c8ec5f02ee902cffd5471f9
Slava Imeshev <imeshev@yahoo.com>
2019-04-02 21:15:34 -0700

    Fixed missing constructors

--- test/src/org/parabuild/ci/webui/agent/status/SATestAgentStatus.java
+++ test/src/org/parabuild/ci/webui/agent/status/SATestAgentStatus.java
@@ -4,6 +4,14 @@ import junit.framework.TestCase;
 
 public final class SATestAgentStatus extends TestCase {
 
+  /**
+   * Constructs a test case with the given name.
+   */
+  public SATestAgentStatus(final String name) {
+    super(name);
+  }
+
+
   public void testToString() {
 
     final AgentStatus agentStatus = new AgentStatus("test.host.name", AgentStatus.ACTIVITY_BUSY, "1.2.5", 12345, ImmutableImage.ZERO_SIZE_IMAGE);
--- test/src/org/parabuild/ci/webui/agent/status/SATestImmutableImage.java
+++ test/src/org/parabuild/ci/webui/agent/status/SATestImmutableImage.java
@@ -4,6 +4,14 @@ import junit.framework.TestCase;
 
 public final class SATestImmutableImage extends TestCase {
 
+  /**
+   * Constructs a test case with the given name.
+   */
+  public SATestImmutableImage(final String name) {
+    super(name);
+  }
+
+
   public void testToString() {
     assertNotNull(ImmutableImage.ZERO_SIZE_IMAGE.toString());
   }
