8b021549a64364cd6b7eeffada077423a1d692d0
Slava Imeshev <imeshev@yahoo.com>
2019-04-06 21:30:51 -0700

    Code quality: added mnissing serialVersionUUID

--- src/org/parabuild/ci/search/SearchManager.java
+++ src/org/parabuild/ci/search/SearchManager.java
@@ -13,27 +13,40 @@
  */
 package org.parabuild.ci.search;
 
-import java.io.*;
-import java.util.*;
-import org.apache.commons.logging.*;
-import org.apache.lucene.document.*;
-import org.apache.lucene.queryParser.*;
-import org.apache.lucene.search.*;
-
-import org.parabuild.ci.archive.*;
-import org.parabuild.ci.common.*;
-import org.parabuild.ci.configuration.*;
-import org.parabuild.ci.error.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.search.Hits;
+import org.parabuild.ci.archive.ArchiveEntry;
+import org.parabuild.ci.archive.ArchiveManager;
+import org.parabuild.ci.archive.ArchiveManagerFactory;
+import org.parabuild.ci.common.StringUtils;
+import org.parabuild.ci.configuration.ConfigurationManager;
 import org.parabuild.ci.error.Error;
-import org.parabuild.ci.object.*;
-import org.parabuild.ci.services.*;
+import org.parabuild.ci.error.ErrorManagerFactory;
+import org.parabuild.ci.object.BuildConfig;
+import org.parabuild.ci.object.BuildRun;
+import org.parabuild.ci.object.ChangeList;
+import org.parabuild.ci.object.StepLog;
+import org.parabuild.ci.object.StepResult;
+import org.parabuild.ci.object.StepRun;
+import org.parabuild.ci.services.SearchService;
+import org.parabuild.ci.services.ServiceManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Provides access to search functionality.
  */
 public final class SearchManager {
 
-  /** @noinspection UNUSED_SYMBOL,UnusedDeclaration*/
+  /**
+   * @noinspection UNUSED_SYMBOL, UnusedDeclaration
+   */
   private static final Log log = LogFactory.getLog(SearchManager.class); // NOPMD
 
   /**
@@ -66,6 +79,21 @@ public final class SearchManager {
 
 
   /**
+   * Helper method to report index request errors to
+   * administrator.
+   *
+   * @param e - exception
+   */
+  private static void reportIndexRequestError(final Exception e) {
+    final Error error = new Error("Log indexing error: " + StringUtils.toString(e));
+    error.setSendEmail(true);
+    error.setDetails(e);
+    error.setSubsystemName(Error.ERROR_SUBSYSTEM_SEARCH);
+    ErrorManagerFactory.getErrorManager().reportSystemError(error);
+  }
+
+
+  /**
    * Search given query String.
    */
   public Hits search(final SearchRequest request) throws IOException, ParseException {
@@ -77,9 +105,9 @@ public final class SearchManager {
     // if buildID available, limit query to the given build.
     final String stringBuildID = request.getParameter(SearchRequestParameter.BUILD_ID);
     if (StringUtils.isValidInteger(stringBuildID)
-      && Integer.parseInt(stringBuildID) != BuildConfig.UNSAVED_ID) {
+            && Integer.parseInt(stringBuildID) != BuildConfig.UNSAVED_ID) {
       searchQuery.append(" +").append(LuceneDocumentFactory.FIELD_BUILD_ID).
-        append(':').append(stringBuildID);
+              append(':').append(stringBuildID);
     }
 
 //    if (log.isDebugEnabled()) log.debug("searchQuery: " + searchQuery);
@@ -136,8 +164,8 @@ public final class SearchManager {
    */
   public void indexChanges(final BuildRun buildRun) {
     try {
-      for (final Iterator i = cm.getBuildRunParticipants(buildRun).iterator(); i.hasNext();) {
-        final ChangeList changeList = (ChangeList)i.next();
+      for (final Iterator i = cm.getBuildRunParticipants(buildRun).iterator(); i.hasNext(); ) {
+        final ChangeList changeList = (ChangeList) i.next();
         final Document document = LuceneDocumentFactory.makeDocument(buildRun, changeList, cm.getChanges(changeList));
         // send index request to indexer queue
         searchService.queueIndexRequest(new DocumentIndexRequest(document));
@@ -160,9 +188,9 @@ public final class SearchManager {
       final ArchiveManager am = ArchiveManagerFactory.getArchiveManager(buildRun.getActiveBuildID());
       // go through the list of entries
       final List entries = am.getArchivedResultEntries(stepResult);
-      for (final Iterator i = entries.iterator(); i.hasNext();) {
+      for (final Iterator i = entries.iterator(); i.hasNext(); ) {
         // make document
-        final String fileName = ((ArchiveEntry)i.next()).getEntryName();
+        final String fileName = ((ArchiveEntry) i.next()).getEntryName();
         final Document document = LuceneDocumentFactory.makeDocument(buildRun, stepResult, fileName);
         // queue
         searchService.queueIndexRequest(new DocumentIndexRequest(document));
@@ -173,25 +201,19 @@ public final class SearchManager {
   }
 
 
-  /**
-   * Helper method to report index request errors to
-   * administrator.
-   *
-   * @param e - exception
-   */
-  private static void reportIndexRequestError(final Exception e) {
-    final Error error = new Error("Log indexing error: " + StringUtils.toString(e));
-    error.setSendEmail(true);
-    error.setDetails(e);
-    error.setSubsystemName(Error.ERROR_SUBSYSTEM_SEARCH);
-    ErrorManagerFactory.getErrorManager().reportSystemError(error);
+  public String toString() {
+    return "SearchManager{" +
+            "searchService=" + searchService +
+            ", cm=" + cm +
+            '}';
   }
-
 
   /**
    * @see SearchService.IndexRequest
    */
   private static final class DocumentIndexRequest implements SearchService.IndexRequest {
+
+    private static final long serialVersionUID = 2991530455552116796L;
 
     private final Document document;
 
@@ -209,13 +231,5 @@ public final class SearchManager {
     public Document getDocumentToIndex() {
       return document;
     }
-  }
-
-
-  public String toString() {
-    return "SearchManager{" +
-            "searchService=" + searchService +
-            ", cm=" + cm +
-            '}';
   }
 }
