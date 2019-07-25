c6f20e571db7130e6b444e462da1c4827f165431
Slava Imeshev <imeshev@yahoo.com>
2019-07-25 11:15:15 -0700

    Fixed an invalid Javadoc reference.

--- src/org/parabuild/ci/webui/SquishLogComponent.java
+++ src/org/parabuild/ci/webui/SquishLogComponent.java
@@ -25,8 +25,6 @@ import java.io.StringReader;
 /**
  * SquishTesterLogComponent is a custom leaf component responsible for
  * displaying build logs presented as unaltered readers.
- *
- * @see BasePage#executePage
  */
 public final class SquishLogComponent extends AbstractXSLRendererComponent {
 
