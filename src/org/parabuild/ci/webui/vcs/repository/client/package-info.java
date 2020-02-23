f5a1762be8db20d5a9b79e85b341dd1e457842fe
Slava Imeshev <imeshev@yahoo.com>
2020-02-22 16:16:53 -0800

    Added a more detailed description of the package.

--- src/org/parabuild/ci/webui/vcs/repository/client/package-info.java
+++ src/org/parabuild/ci/webui/vcs/repository/client/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Client-side source files and subpackages.
+ * Client-side source files and subpackages for managing servers and repositories.
  */
 package org.parabuild.ci.webui.vcs.repository.client;
