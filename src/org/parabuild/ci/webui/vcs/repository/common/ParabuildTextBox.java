9ad67fbdb4cf9219bf0e134eeaac0973e7aa31bc
Slava Imeshev <imeshev@yahoo.com>
2020-04-15 23:58:01 -0700

    Code quality: cleaned up Java docs.

--- src/org/parabuild/ci/webui/vcs/repository/common/ParabuildTextBox.java
+++ src/org/parabuild/ci/webui/vcs/repository/common/ParabuildTextBox.java
@@ -4,12 +4,13 @@ import com.google.gwt.user.client.ui.TextBox;
 import org.parabuild.ci.common.HasInputValue;
 
 /**
- * Reusable text box field.
+ * Reusable text box field that supports {@link HasInputValue}.
  */
 public class ParabuildTextBox extends TextBox implements HasInputValue {
 
   /**
-   * Creates an empty text box.
+   * Creates an empty text box of data size <code>maxLength</code> characters and visible length of
+   * <code>visibleLength</code> characters.
    */
   public ParabuildTextBox(final int maxLength, final int visibleLength) {
 
@@ -20,18 +21,21 @@ public class ParabuildTextBox extends TextBox implements HasInputValue {
 
   @Override
   public void setInputValue(final String value) {
+
     setValue(value);
   }
 
 
   @Override
   public boolean isInputEditable() {
+
     return isEnabled();
   }
 
 
   @Override
   public String getInputValue() {
+
     return getValue();
   }
 }
