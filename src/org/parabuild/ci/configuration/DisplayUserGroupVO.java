89822b56e9af156bf0674121c741421a80bec85d
Slava Imeshev <imeshev@yahoo.com>
2020-04-27 20:18:15 -0700

    Code quality: cleaned up Java docs.

--- src/org/parabuild/ci/configuration/DisplayUserGroupVO.java
+++ src/org/parabuild/ci/configuration/DisplayUserGroupVO.java
@@ -13,10 +13,10 @@
  */
 package org.parabuild.ci.configuration;
 
-import java.io.*;
-import java.util.*;
+import org.parabuild.ci.object.Group;
 
-import org.parabuild.ci.object.*;
+import java.io.Serializable;
+import java.util.Comparator;
 
 /**
  * Value object to hold displayable user data.
